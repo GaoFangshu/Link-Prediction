a66db32d7a9df5fc9d9e1128d0bdc9ff1b1a70e4
GaoFangshu <gaofangshu@foxmail.com>
2018-06-02 12:56:14 +0800

    Try to get dice similarity

--- graph.py
+++ graph.py
@@ -7,20 +7,12 @@ data.sample(prop=1)
 data.prepare_data()
 data.init_graph()
 
-#
-# def get_ddirect(ids):
-#     # input: int: id1 and id2
-#     # output: tuple: (from_id, to_id)
-#     # year(from_id) >= year(to_id)
-#     print(ids)
-#     year_id1 = data.data_node_info["year"][data.data_node_info["id"] == ids[0]].iloc[0]   # TODO: risky because id may not unique
-#     year_id2 = data.data_node_info["year"][data.data_node_info["id"] == ids[1]].iloc[0]
-#     print(year_id1)
-#     print(year_id2)
-#     if year_id1 >= year_id2:  # TODO: how to deal with papers in same year, I ignore it now
-#         return (ids[0], ids[1])
-#     else:
-#         return (ids[1], ids[0])
+data.graph
+
+data.graph.pagerank()
+
+data.graph.similarity_dice(mode="OUT", loops=False)
+
 
 edges = data.data_train[["id_source", "id_target"]].iloc[0:3].apply(data.get_direct, axis=1)
 
@@ -28,6 +20,9 @@ edges = data.data_train[["id_source", "id_target"]].iloc[0:3].apply(data.get_dir
 edges.tolist()
 
 g=igraph.Graph(directed=True)
+
+
+
 
 g.add_vertices(list(data.node_dict.keys()))
 g.add_edges(edges.tolist())
